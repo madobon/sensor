c53232c3a08957e7b5bced6f31e4e2a7b4807c86
madobon <mimicopy@live.jp>
2015-12-05 16:38:18 +0900

    add websocket

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,18 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 
-gulp.task('default', () => {
+// html
+gulp.task('html', function () {
+  return gulp.src('src/**.html')
+    .pipe(gulp.dest('dist'));
+});
+
+// babel
+gulp.task('babel', () => {
   return gulp.src('src/**.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
+
+// default
+gulp.task('default', ['html', 'babel']);
